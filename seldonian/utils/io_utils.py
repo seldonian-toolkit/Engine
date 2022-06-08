d7ac424b417671195831bb40ed5a29f98e638862
Austin Hoag <austinthomashoag@gmail.com>
2022-06-08 13:16:07 -0700

    document utils

--- seldonian/utils/io_utils.py
+++ seldonian/utils/io_utils.py
@@ -2,19 +2,34 @@ import os
 import pickle
 import json
 
-def dir_path(string):
-    if os.path.isdir(string):
-        return string
+def dir_path(path):
+    """ A utility for checking whether a path is a directory
+
+    :param path: An input path that may or may not be a directory
+    :type path: str
+    """
+    if os.path.isdir(path):
+        return path
     else:
-        raise NotADirectoryError(string)
+        raise NotADirectoryError(path)
 
 
 def load_pickle(filename):
+    """ A wrapper for loading an object from a pickle file
+
+    :param filename: A filename pointing to a pickle file
+    :type filename: str
+    """
     with open(filename,'rb') as infile:
         data = pickle.load(infile)
     return data
 
 def load_json(filename):
+    """ A wrapper for loading an object from a JSON file
+
+    :param filename: An input filename pointing to a JSON file
+    :type filename: str
+    """
     with open(filename,'r') as infile:
         data = json.load(infile)
     return data
--- seldonian/utils/stats_utils.py
+++ seldonian/utils/stats_utils.py
@@ -5,6 +5,12 @@ def stddev(v):
     """
     Sample standard deviation of the vector v,
     with Bessel's correction
+
+    :param v: vector of data
+    :type v: Numpy ndarray
+
+    :return: Standard deviation with Bessel's correction
+    :rtype: float
     """
     return np.std(v,ddof=1)         
 
@@ -15,18 +21,59 @@ def tinv(p, nu):
     probabilities in p. 
     Python implementation of Matlab's tinv function:
      https://www.mathworks.com/help/stats/tinv.html
+
+    :param p: Probability
+    :type p: float
+
+    :param nu: Degrees of freedom
+    :type nu: int
+
+    :return: Inverse of the Student's t CDF
+    :rtype: float
     """
     return t.ppf(p, nu)
 
 def weighted_sum_gamma(arr,gamma=0.9):
-    # print(arr,gamma)
-    """ Calculate weighted sum of arr,
-    where weights are gamma**(index of arr)  """
+    """ Calculate weighted sum of an array,
+    where weights are gamma**(index of arr). 
+    Used in calculating sum of weighted rewards in RL
+
+    :param arr: An input array
+    :type arr: Numpy ndarray
+
+    :param gamma: The constant used for weighting the array
+    :type gamma: float  
+
+    :return: The weighted sum
+    :rtype: float  
+    """
     weights = np.power(gamma,range(len(arr)))
     return np.average(arr,weights=weights)*np.sum(weights)
 
 
 def generate_data(numPoints,loc_X=0.0,loc_Y=0.0,sigma_X=1.0,sigma_Y=1.0):
+    """ Generate 2D random normal data
+    
+    :param numPoints: The number of data points to generate
+    :type numPoints: int
+
+    :param loc_X: The center of the normal distribution 
+        in the X dimension
+    :type loc_X: float
+
+    :param loc_Y: The center of the normal distribution 
+        in the Y dimension
+    :type loc_Y: float
+
+    :param sigma_X: The standard deviation of the normal distribution 
+        in the X dimension
+    :type sigma_X: float
+
+    :param sigma_Y: The standard deviation of the normal distribution 
+        in the Y dimension
+    :type sigma_Y: float
+    """
+
     X =     np.random.normal(loc_X, sigma_X, numPoints) # Sample x from a standard normal distribution
     Y = X + np.random.normal(loc_Y, sigma_Y, numPoints) # Set y to be x, plus noise from a standard normal distribution
     return (X,Y)
