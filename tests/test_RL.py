7d598002ec518fca82bda2e8e4ef1045042a7e21
james <jekostas@umass.edu>
2022-08-11 15:00:22 -0400

    added docstrings and comments to test_RL.py

--- tests/test_RL.py
+++ tests/test_RL.py
@@ -13,11 +13,14 @@ def test_tables():
     num_actions = 3
     mytable = Q_Table(min_state, num_states, num_actions)
 
+    #check from_environment_state_to_0_indexed_state()
     assert mytable.from_environment_state_to_0_indexed_state(-3) == 0
     assert mytable.from_environment_state_to_0_indexed_state(2) == 5
 
+    #check initialization
     assert np.allclose(mytable.get_action_values_given_state(-3), np.array([0.0, 0.0, 0.0]))
 
+    #check manually setting weights, and get_action_values_given_state()
     mytable.weights[0, 0] = 1.1
     mytable.weights[1, 0] = 2.2
     mytable.weights[2, 0] = -3.3
@@ -31,6 +34,7 @@ def test_tables():
     assert np.allclose(mytable.get_action_values_given_state(0), np.array([1.1, 2.2, -3.3]))
 
 def test_Discrete_Action_Policy():
+    """test Discrete_Action_Policy action index methods"""
     min_action = -1
     num_actions = 3
     p = Discrete_Action_Policy(min_action, num_actions)
@@ -44,12 +48,14 @@ def test_Discrete_Action_Policy():
     assert p.from_0_indexed_action_to_environment_action(2) == 1
 
 def test_Softmax():
+    """test Softmax functions"""
     sm = Softmax(-1, 1)
     e_to_something_stable = np.array([0.1108031584, 0.0040867714, 1.0])
     assert np.allclose(sm.get_e_to_the_something_terms([1.1, -2.2, 3.3]), e_to_something_stable)
     assert np.allclose(sm.get_action_probs_from_action_values([1.1, -2.2, 3.3]), e_to_something_stable / sum(e_to_something_stable))
 
 def test_Parameterized_non_learning_softmax_agent():
+    """test Parameterized_non_learning_softmax_agent"""
     observation_space = Discrete_Space(-1, 2)
     action_space = Discrete_Space(-1, 1)
     env_desc = Env_Description(observation_space, action_space)
@@ -62,8 +68,9 @@ def test_Parameterized_non_learning_softmax_agent():
 
     new_params = np.random.rand(correct_shape[0], correct_shape[1])
     agent.set_new_params(new_params)
-    assert np.allclose(agent.get_params(), new_params)
+    assert np.allclose(agent.get_params(), new_params) #test set_new_params() and get_params()
 
+    #test get_action_values() and get_prob_this_action()
     agent.set_new_params(np.array([[1.1, 1.1, 1.1, 1.1], [-2.2, -2.2, -2.2, -2.2], [3.3, 3.3, 3.3, 3.3]]))
     for state in range(-1, 3):
         assert np.allclose(agent.get_action_values(state), [1.1, -2.2, 3.3])
@@ -74,9 +81,10 @@ def test_Parameterized_non_learning_softmax_agent():
     incorrect_shape = (4, 3)
     bad_params = np.random.rand(incorrect_shape[0], incorrect_shape[1])
     with pytest.raises(Exception):
-        agent.set_new_params(bad_params)
+        agent.set_new_params(bad_params) #make sure throws error with bad shape
 
 def test_spaces_and_env_descriptions():
+    """test Continuous_Space constructor and methods"""
     cont_space = Continuous_Space(np.array([[0.0, 1.1], [3.3, 4.4], [5.5, 6.6]])) #should be no error
     with pytest.raises(Exception):
         cont_space = Continuous_Space(np.array([[0.0, 1.1], [3.3, 4.4], [5.5, -6.6]])) #max is smaller than min
