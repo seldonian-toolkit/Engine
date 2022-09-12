596ee67b11a1f148292ca5461e5a5cf6f9241055
Austin Hoag <austinthomashoag@gmail.com>
2022-09-12 13:50:42 -0700

    update pypi version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="seldonian_engine",
-    version="0.4.1",
+    version="0.4.2",
     author="Austin Hoag",
     author_email="austinthomashoag@gmail.com",
     description="Core library for Seldonian algorithms",
