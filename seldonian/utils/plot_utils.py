52ca9123d8cee4d05f8daab1e30521eab8a33950
Austin Hoag <austinthomashoag@gmail.com>
2022-09-13 14:51:45 -0700

    plotting function reflects updates to gradient descent solution dict

--- seldonian/utils/plot_utils.py
+++ seldonian/utils/plot_utils.py
@@ -31,16 +31,15 @@ def plot_gradient_descent(solution,primary_objective_name,
     """
     
     fontsize=10
-    solution_found = solution['solution_found']
     theta_vals = solution['theta_vals'] 
     lamb_vals = solution['lamb_vals'] # i x j array where i is number of iterations, j is number of constraints
     f_vals = solution['f_vals'] # length = i array where i is number of iterations 
     g_vals = solution['g_vals'] # i x j array where i is number of iterations, j is number of constraints
     L_vals = solution['L_vals']
-    if solution_found:
-        best_index = solution['best_index']
-        best_feasible_f = solution['best_feasible_f']
-        best_feasible_g = solution['best_feasible_g']
+    
+    best_index = solution['best_index']
+    best_f = solution['best_f']
+    best_g = solution['best_g']
     n_constraints = g_vals[0].shape[0]
     n_cols = 4 
     # fig = plt.figure(figsize=(10,4+(n_constraints-1)*2))
@@ -59,9 +58,8 @@ def plot_gradient_descent(solution,primary_objective_name,
         ax_f.plot(np.arange(len(f_vals)),f_vals,linewidth=2)
         ax_f.set_xlabel("Iteration")
         ax_f.set_ylabel(rf"$\hat{{f}}(\theta)$: {primary_objective_name}",fontsize=fontsize)
-        if solution_found:
-            ax_f.axvline(x=best_index,linestyle='--',color='k')
-            ax_f.axhline(y=best_feasible_f,linestyle='--',color='k')
+        ax_f.axvline(x=best_index,linestyle='--',color='k')
+        ax_f.axhline(y=best_f,linestyle='--',color='k')
 
         # Subplot: lambda[constraint_index]
         ax_lamb = axes_this_constraint[1]
@@ -70,10 +68,10 @@ def plot_gradient_descent(solution,primary_objective_name,
             linewidth=2)
         ax_lamb.set_xlabel("Iteration")
         ax_lamb.set_ylabel(rf"$\lambda_{row_number}$",fontsize=fontsize)
-        if solution_found:
-            ax_lamb.axvline(x=best_index,linestyle='--',color='k')
-            best_feasible_lamb = lamb_vals[best_index][constraint_index]
-            ax_lamb.axhline(y=best_feasible_lamb,linestyle='--',color='k')
+
+        ax_lamb.axvline(x=best_index,linestyle='--',color='k')
+        best_lamb = lamb_vals[best_index][constraint_index]
+        ax_lamb.axhline(y=best_lamb,linestyle='--',color='k')
 
         # Subplot: g[constraint_index]
         ax_g = axes_this_constraint[2]
@@ -84,9 +82,8 @@ def plot_gradient_descent(solution,primary_objective_name,
         ax_g.set_ylabel(rf"$\mathrm{{HCUB}}(\hat{{g}}_{{{row_number}}})$",fontsize=fontsize)
         ax_g.fill_between(np.arange(len(g_vals)),0.0,1e6,color='r',zorder=0,alpha=0.5)
         ax_g.set_ylim(min(-0.25,min(g_vals_this_constraint)),max(0.25,max(g_vals_this_constraint)*1.2))
-        if solution_found:
-            ax_g.axvline(x=best_index,linestyle='--',color='k')
-            ax_g.axhline(y=best_feasible_g[constraint_index],linestyle='--',color='k')
+        ax_g.axvline(x=best_index,linestyle='--',color='k')
+        ax_g.axhline(y=best_g[constraint_index],linestyle='--',color='k')
         ax_g.set_xlim(0,len(g_vals))
 
         # Subplot: Lagrangian, repeated for each row
@@ -94,10 +91,9 @@ def plot_gradient_descent(solution,primary_objective_name,
         ax_L.plot(np.arange(len(L_vals)),L_vals,linewidth=2)
         ax_L.set_xlabel("Iteration")
         ax_L.set_ylabel(r"$L(\theta,\lambda)$",fontsize=fontsize)
-        if solution_found:
-            ax_L.axvline(x=best_index,linestyle='--',color='k')
-            best_feasible_L = L_vals[best_index]
-            ax_L.axhline(y=best_feasible_L,linestyle='--',color='k')
+        ax_L.axvline(x=best_index,linestyle='--',color='k')
+        best_L = L_vals[best_index]
+        ax_L.axhline(y=best_L,linestyle='--',color='k')
     
     title = rf"KKT optimization for $L(\theta,\lambda) = \hat{{f}}(\theta) + \sum_{{k=1}}^{{{n_constraints}}}{{\lambda_k}} \mathrm{{HCUB}}(\hat{{g}}_k(\theta)) $"
     plt.suptitle(title)
