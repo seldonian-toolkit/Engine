ba2db93c6bff28d5d0ee63bc28a22c8950aaddfb
httse9 <62588559+httse9@users.noreply.github.com>
2023-05-05 05:17:31 -0400

    infer std fix bug
    make sure proposed std > 0

--- seldonian/parse_tree/mcmc/proposal.py
+++ seldonian/parse_tree/mcmc/proposal.py
@@ -4,9 +4,17 @@ class ProposalDistribution:
     """
     Normal proposal distribution
     """
-    def __init__(self, proposal_width):
+    def __init__(self, proposal_width, infer_std):
         self.proposal_width = proposal_width
+        self.infer_std = infer_std
 
     def propose(self, x):
         # supports multidimension x
-        return np.random.normal(loc=x, scale=self.proposal_width)
+        proposal = np.random.normal(loc=x, scale=self.proposal_width)
+
+        if self.infer_std:
+            # make sure std > 0
+            if proposal[1] <= 0:
+                proposal[1] = 1e-10
+        
+        return proposal
