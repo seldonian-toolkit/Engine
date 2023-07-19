1f36e8a09d04121aff2a2bdd4476923e2daf824e
austinhoag <austinthomashoag@gmail.com>
2023-07-19 14:01:47 -0700

    starting support for agent objects, alt rewards in RL_runner

--- seldonian/RL/RL_runner.py
+++ seldonian/RL/RL_runner.py
@@ -1,3 +1,5 @@
+import inspect
+
 from seldonian.RL.Agents.Discrete_Random_Agent import *
 from seldonian.RL.Agents.mountain_car_rough_solution import *
 from seldonian.RL.Agents.Parameterized_non_learning_softmax_agent import *
@@ -6,7 +8,6 @@ from seldonian.RL.Agents.keyboard_gridworld import *
 from seldonian.RL.environments.gridworld import *
 from seldonian.RL.environments.mountaincar import *
 from seldonian.RL.environments.n_step_mountaincar import *
-from seldonian.RL.environments.simglucose_env import *
 
 from seldonian.dataset import Episode
 
@@ -49,10 +50,13 @@ def run_trial(
     episodes = []
     num_episodes = hyperparameter_and_setting_dict["num_episodes"]
     print(f"Have {num_episodes} episodes in trial")
-    agent = create_agent(hyperparameter_and_setting_dict)
+    if type(hyperparameter_and_setting_dict["agent"]) == str:
+        agent = create_agent(hyperparameter_and_setting_dict)
+    else:
+        print("Agent is directly provided")
+        agent = hyperparameter_and_setting_dict["agent"]
+    
     if model_params is not None:
-        # print("Setting new model params:")
-        # print(model_params)
         agent.set_new_params(model_params)
 
     env = hyperparameter_and_setting_dict["env"]
@@ -82,6 +86,8 @@ def run_trial_given_agent_and_env(agent, env, num_episodes):
     """A wrapper for run_trial() where parameters
     are specified explicity rather than via a dictionary.
 
+    Not parallelized here because often agent or env is not pickleable.
+
     :param agent: RL Agent
     :param env: RL Environment
     :param num_episodes: Number of episodes to run
@@ -107,6 +113,53 @@ def run_episode(agent, env):
     observations = []
     actions = []
     rewards = []
+    alt_rewards = []
+    prob_actions = []
+
+    env.reset()
+    observation = env.get_observation()
+    while not env.terminated():
+        action = agent.choose_action(observation)
+        print(f"cr,cf = {action}")
+        reward = env.transition(action)
+        if type(reward) == tuple:
+            primary_reward = reward[0]
+            alt_rewards_this_step = reward[1:]
+            alt_rewards.append(alt_rewards_this_step)
+        else:
+            primary_reward = reward
+        next_observation = env.get_observation()
+        agent.update(observation, next_observation, primary_reward, env.terminated())
+
+        observations.append(observation)
+        actions.append(action)
+        rewards.append(primary_reward)
+        prob_actions.append(agent.get_prob_this_action(observation, action))
+
+        observation = next_observation
+
+    return Episode(observations, actions, rewards, prob_actions, np.array(alt_rewards))
+
+
+def run_episode_from_dict(hyperparameter_and_setting_dict, model_params=None):
+    """Run a single episode
+
+    :param agent: RL Agent
+    :param env: RL Environment
+
+    :return: RL Episode
+    :rtype: :py:class:`.Episode`
+    """
+    env = hyperparameter_and_setting_dict["env"]
+    env_desc = env.get_env_description()
+    agent = create_agent(hyperparameter_and_setting_dict)
+    if model_params is not None:
+        # print("Setting new params")
+        # set agent's weights to the trained model weights
+        agent.set_new_params(model_params)
+    observations = []
+    actions = []
+    rewards = []
     prob_actions = []
 
     env.reset()
@@ -129,8 +182,8 @@ def run_episode(agent, env):
     return Episode(observations, actions, rewards, prob_actions)
 
 
-def run_episode_from_dict(hyperparameter_and_setting_dict, model_params=None):
-    """Run a single episode
+def run_episode_par(create_env_func,create_agent_func):
+    """Run a single episode. A function that can be parallelized. 
 
     :param agent: RL Agent
     :param env: RL Environment
@@ -138,13 +191,9 @@ def run_episode_from_dict(hyperparameter_and_setting_dict, model_params=None):
     :return: RL Episode
     :rtype: :py:class:`.Episode`
     """
-    env = hyperparameter_and_setting_dict["env"]
-    env_desc = env.get_env_description()
-    agent = create_agent(hyperparameter_and_setting_dict)
-    if model_params is not None:
-        # print("Setting new params")
-        # set agent's weights to the trained model weights
-        agent.set_new_params(model_params)
+    env = create_env_func()
+    agent = create_agent_func()
+    
     observations = []
     actions = []
     rewards = []
